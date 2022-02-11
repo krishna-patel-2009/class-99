0555aa3b174970a60bbc3dcad899805eac3544b2
krishna-patel-2009 <98408680+krishna-patel-2009@users.noreply.github.com>
2022-02-11 20:35:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function run(event)
+{
+    console.log(event);
+    var contant = event.results[0][0].transcript;
+    console.log(contant);
+    document.getElementById("textbox").innerHTML = contant;
+    speak();
+}
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value ;
+    var utterthis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+}
+Webcam.set({
+    width:360,    
+    height:250,
+    image_format:"png",
+    png_quality:90
+});
+camera = document.getElementById("camera");
